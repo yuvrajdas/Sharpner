4f2b0c8ada10e18bb5423f9d0d76bd87bab4eedd
yuvrajdas <yuvrajdas1531@gmail.com>
2024-02-07 22:07:18 +0530

    state value for date amount and title

--- React/creating_your_own_first_component/src/ExpenseForm.jsx
+++ React/creating_your_own_first_component/src/ExpenseForm.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpensForm.css'
 const ExpenseForm = () => {
 
-
+    const [title, setTitle] =  useState("")
+    const [amount, setAmount] =  useState("")
+    const [date, setDate] =  useState("")
     return (
         <>
             <div className='form-wrapper mt-3'>
                 <div className="row ps-4 pe-4 pt-2 pb-3">
                 <div class="mb-3 col-md-6">
                     <label for="Title" class="form-label">Title</label>
-                    <input type="text" class="form-control" />
+                    <input type="text" class="form-control" onChange={(e)=>setTitle(e.target.value)} />
                 </div>
                 <div class="mb-3 col-md-6">
                     <label for="Amount" class="form-label">Amount</label>
-                    <input type="text" class="form-control"/>
+                    <input type="text" class="form-control" onChange={(e)=>setAmount(e.target.value)}/>
                 </div>
                 <div class="mb-3 col-md-6">
-                    <label for="Amount" class="form-label">Amount</label>
-                    <input type="date" class="form-control"/>
+                    <label for="Amount" class="form-label">Date</label>
+                    <input type="date" class="form-control" onChange={(e)=>setDate(e.target.value)}/>
                 </div>
                 </div>
                 <button type="submit" class="btn btn-primary w-25 float-end me-3">Add Expense</button>
-               
+            </div>
+
+            <div>
+                <p> title:{title} Amount:{amount} Date:{date}</p>
             </div>
         </>
     )
