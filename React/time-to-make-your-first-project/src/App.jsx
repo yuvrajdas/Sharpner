5ac28f06c9f3c43e92e5bc39333115b808b5add4
yuvrajdas <yuvrajdas1531@gmail.com>
2024-02-13 22:49:17 +0530

    Time to make your first project

---
+++ React/time-to-make-your-first-project/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const listItemStyle = {
+    border: '1px solid', borderRadius: '5px', padding: '5px'
+  };
+
+  const [user, setUser] = useState({ username: "", age: "" });
+  const [userList, setUserList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const addUser = (e) => {
+    e.preventDefault();
+    if (!isFormValid()) {
+      return;
+    }
+    if (user.age < 0) {
+      setErrorMessage("Age cannot be negative");
+      return;
+    }
+    setUserList([...userList, user]); // Create a new array with the updated user list
+    setUser({ username: "", age: "" }); // Clear the input fields after adding the user
+    setErrorMessage(""); // Clear error message if it was set previously
+  }
+
+  const isFormValid = () => {
+    return user.username !== "" && user.age !== "";
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="row mt-3">
+          <div className="col-md-8 m-auto">
+            <div className="card">
+              <form className="p-4" onSubmit={addUser}>
+                <div className="mb-3">
+                  <label htmlFor="username" className="form-label">Username</label>
+                  <input type="text" value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} className="form-control shadow-none" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="age" className="form-label">Age (Years)</label>
+                  <input type="number" value={user.age} onChange={(e) => setUser({ ...user, age: e.target.value })} className="form-control shadow-none" />
+                </div>
+                <button type="submit" className="btn btn-success">Add User</button>
+                {errorMessage && <div className="text-danger mt-2">{errorMessage}</div>}
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="col-md-8 m-auto">
+            <div className="card p-4">
+              {userList?.map((ele, index) => {
+                return (<div style={listItemStyle} key={index}>
+                  <span>{ele.username}</span> <span>({ele.age} years old)</span>
+                </div>)
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
