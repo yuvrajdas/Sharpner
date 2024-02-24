ace51c1d6a4c368ae072de671845fbd2bc1bc24c
yuvrajdas <yuvrajdas1531@gmail.com>
2024-02-24 12:34:56 +0530

    Making the Items visible

--- React/creating-a-restaurant-website/src/App.js
+++ React/creating-a-restaurant-website/src/App.js
@@ -4,9 +4,12 @@ import Navbar from './layout/Navbar';
 import Home from './view-components/Home';
 function App() {
   return (
-    <>
+    <> 
+      <div className='mb-5'>
       <Navbar />
       <Home />
+      </div>
+    
     </>
   );
 }
--- React/creating-a-restaurant-website/src/view-components/Home.jsx
+++ React/creating-a-restaurant-website/src/view-components/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import bannerImage from '../assets/images/meals.jpg'; 
-
+import React, { useState } from 'react';
+import bannerImage from '../assets/images/meals.jpg';
+import { DUMMY_MEALS } from '../dummyMeals';
 const Home = () => {
+
   return (
     <>
-    <div>
+      <div className='pb-5'>
         <img className='banner' src={bannerImage} alt="Banner" />
         <div className='summary-card'>
           <div className='summary-card__heading'>Delicious Food, Delivered To You</div>
@@ -12,11 +13,25 @@ const Home = () => {
             Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.
           </p>
           <p className='summary-card__sub-heading'>
-          Our meals feature high-quality, just-in-time cooking by experienced chefs, ensuring every dish exceeds expectations with premium ingredients.
+            Our meals feature high-quality, just-in-time cooking by experienced chefs, ensuring every dish exceeds expectations with premium ingredients.
           </p>
         </div>
-    </div>
-
+        <div className='item-card'>
+          {
+            DUMMY_MEALS.map((meals) => {
+              return (
+                <div className="item-card_wrapper">
+                  <div className='item-card__heading'>{meals.name}</div>
+                  <div className='item-card__desc'>{meals.description}</div>
+                  <div className='item-card__price'> ${meals.price}</div>
+                  <hr />
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      
     </>
   );
 }
